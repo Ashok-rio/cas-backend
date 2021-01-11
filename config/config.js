cdc9f7c23ab6358422eab4db4ced43e5221000d1
Ashok <ashokask186@gmail.com>
2021-01-12 04:00:53 +0530

    change mongodb url

--- config/config.js
+++ config/config.js
@@ -1,16 +1,18 @@
-require('dotenv').config();//instatiate environment variables
+require("dotenv").config(); //instatiate environment variables
 
-let CONFIG = {} //Make this global to use all over the application
+let CONFIG = {}; //Make this global to use all over the application
 
-CONFIG.app = process.env.APP || 'development'
+CONFIG.app = process.env.APP || "development";
 
-CONFIG.port = process.env.PORT || '3000'
+CONFIG.port = process.env.PORT || "3000";
 
-// CONFIG.db_uri  = process.env.MONGODB_URI   || 'mongodb+srv://ashok123:7rxfg9KMURQfCukl@anywhere.fhsyn.gcp.mongodb.net/anywhere?retryWrites=true&w=majority';
-CONFIG.db_uri  = process.env.MONGODB_URI   || 'mongodb://localhost:27017/cas_mobile'
+CONFIG.db_uri =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://ashok123:7rxfg9KMURQfCukl@anywhere.fhsyn.gcp.mongodb.net/anywhere?retryWrites=true&w=majority";
+// CONFIG.db_uri  = process.env.MONGODB_URI   || 'mongodb://localhost:27017/cas_mobile'
 
-CONFIG.jwt_encryption = process.env.JWT_ENCRYPTION || 'jwt_please_change'
+CONFIG.jwt_encryption = process.env.JWT_ENCRYPTION || "jwt_please_change";
 
-CONFIG.jwt_expiration = process.env.JWT_EXPIRATION || '10000'
+CONFIG.jwt_expiration = process.env.JWT_EXPIRATION || "10000";
 
 module.exports = CONFIG;
